77f75c24d77e85a773ad628448d4ce08c6d5241a
Pavel Eshevskiy <Pashkas123@yandex.ru>
2023-02-23 21:55:30 +0300

    We have worked out this first seminar

---
+++ seminars/seminar1/Program.cs
@@ -0,0 +1,23 @@
+﻿//Console.WriteLine("Input first number");
+//int num1 = Convert.ToInt32(Console.ReadLine()); 
+//Console.WriteLine("Input second number");
+//int num2 = Convert.ToInt32(Console.ReadLine()); 
+
+//if( num2 * num2 == num1)
+//{
+  //  Console.WriteLine($"First number {num1} is quad of second number {num2}");
+//}
+//else
+//{
+   // Console.WriteLine($"First number {num1} is not quad of second number {num2}");
+//}
+
+Console.WriteLine("Input N");
+int n = Convert.ToInt32(Console.ReadLine()); 
+
+int current = n * (-1);
+while(current <= n)
+{
+    Console.Write(current + " ");
+    current++;
+}
