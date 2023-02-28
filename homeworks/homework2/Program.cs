12f34f619a3634857494280d888c1833f59dc23f
Pavel Eshevskiy <Pashkas123@yandex.ru>
2023-02-28 23:08:55 +0300

    completed homework2

--- homeworks/homework2/Program.cs
+++ homeworks/homework2/Program.cs
@@ -4,18 +4,49 @@
 //782 -> 8
 //918 -> 1
 
-int SecondDigit(int num)
+//int SecondDigit(int num)
+//{
+   // return num % 100 / 10;
+//}
+
+//Console.WriteLine("Input three-digit number");
+//int num = Convert.ToInt32(Console.ReadLine());
+
+//if( num > 99 && num <= 999)
+//{
+    //Console.WriteLine(SecondDigit(num));
+//}
+//else 
+//{
+    //Console.WriteLine("is not three-digit number");
+//}
+
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+//или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+int ThirdDigit( int num)
 {
-    if( num > 99 && num <= 999)
-    return num % 100 / 10;
-    else 
-    return Console.WriteLine("is not three-digit number");
+    while( num > 999 )
+    {
+        num /= 10;
+    }
+    return num % 10;
 }
 
-Console.WriteLine("Input three-digit number");
+Console.WriteLine("Input number");
 int num = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine(SecondDigit(num));
+if( num < 100 )
+{
+    Console.WriteLine("No third digit");
+}
+else
+{
+    Console.WriteLine(ThirdDigit(num));
+}
 
 
 //Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день 
